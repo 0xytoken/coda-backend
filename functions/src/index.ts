92a2039c965a2ce124c341aa98d057c263dc9bfb
Niklas Benjamin Muegge <69041350+nmuegge@users.noreply.github.com>
2022-06-15 10:21:55 +0700

    modified:   index.ts

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,52 +3,47 @@ import * as ax from "axios";
 
 const axios = ax.default;
 
-exports.httpRequest = functions
-    .https.onRequest((req, res) => {
-      try {
-        const body = JSON.parse(req.body);
-        switch (body.method) {
-          case "POST":
-            console.log("POST");
-            console.log(body.url);
-            console.log("body.body "+body.body);
-            axios
-                .post(body.url, body.body)
-                .then((response: ax.AxiosResponse) => {
-                  console.log(JSON.stringify(response.data));
-                  /* response.data.requestFromIp = req.ip;
-                response.data.firebaseIP = address.ip(); */
-                  res.end(JSON.stringify(response.data));
-                })
-                .catch((error: ax.AxiosError) => {
-                  console.error(JSON.stringify(error));
-                  res.status(401).end(req.ip);
-                });
-            return;
-          case "GET":
-            console.log("GET");
-            axios
-                .get(body.url, body.body)
-                .then((response: ax.AxiosResponse) => {
-                  console.log(JSON.stringify(response.data));
-                  /* response.data.requestFromIp = req.ip;
-                response.data.firebaseIP = address.ip(); */
-                  res.end(response.data);
-                })
-                .catch((error: ax.AxiosError) => {
-                  console.error(JSON.stringify(error));
-                  res.status(402).end(req.ip);
-                });
-            return;
-          default:
-            res.status(403).end(req.ip);
-            return;
-        }
-      } catch (e) {
-        res.status(404).end(req.ip);
-        console.log(e);
-      }
-    });
+exports.httpRequest = functions.https.onRequest((req, res) => {
+  try {
+    const body = JSON.parse(req.body);
+    switch (body.method) {
+      case "POST":
+        console.log("POST");
+        console.log(body.url);
+        console.log("body.body " + body.body);
+        axios
+            .post(body.url, body.body)
+            .then((response: ax.AxiosResponse) => {
+              console.log(JSON.stringify(response.data));
+              res.end(JSON.stringify(response.data));
+            })
+            .catch((error: ax.AxiosError) => {
+              console.error(JSON.stringify(error));
+              res.status(401).end(req.ip);
+            });
+        return;
+      case "GET":
+        console.log("GET");
+        axios
+            .get(body.url, body.body)
+            .then((response: ax.AxiosResponse) => {
+              console.log(JSON.stringify(response.data));
+              res.end(response.data);
+            })
+            .catch((error: ax.AxiosError) => {
+              console.error(JSON.stringify(error));
+              res.status(402).end(req.ip);
+            });
+        return;
+      default:
+        res.status(403).end(req.ip);
+        return;
+    }
+  } catch (e) {
+    res.status(404).end(req.ip);
+    console.log(e);
+  }
+});
 
 exports.jsonToCodaSchema = functions.https.onRequest((req, res) => {
   let code = "coda.makeObjectSchema({properties: {";
@@ -57,6 +52,14 @@ exports.jsonToCodaSchema = functions.https.onRequest((req, res) => {
   res.end(code);
 });
 
+/**
+ * Adds two numbers together.
+ * @param {Object} data the JSON object to be converted to the coda Schema.
+ * @param {boolean} shouldIncludeEmptyArrays whether empty Arrays should be
+ * included.If this is set to 'true' you will have to manually adjust the
+ * result to specify the type of Array.
+ * @return {String} Your generated Coda Scheme.
+ */
 function dataRecursion(
     data: object,
     shouldIncludeEmptyArrays: boolean
